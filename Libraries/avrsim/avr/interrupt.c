4fe8fa0b14cd9d0a42adf66625560227b57d6e61
mars <martin@rebel-it.com>
2012-07-01 20:14:34 +0100

    code used for unit testing firmware

--- Libraries/avrsim/avr/interrupt.c
+++ Libraries/avrsim/avr/interrupt.c
@@ -1,4 +1,11 @@
 #include "interrupt.h"
+#include "io.h"
 
-void sei(void){}
-void cli(void){}
+void sei(void){
+  // enable global interrupts: set Status Register flag I
+  SREG |= _BV(7);
+}
+
+void cli(void){
+  SREG &= ~_BV(7);
+}
--- Libraries/avrsim/avr/io.h
+++ Libraries/avrsim/avr/io.h
@@ -4,13 +4,7 @@
 #include <inttypes.h>
 #include <avr/iomx8.h>
 #include <avr/portpins.h>
-
-/* #elif defined (__AVR_ATmega168__) */
-/* #  include <avr/iom168.h> */
-/* #elif defined (__AVR_ATmega168P__) */
-/* #  include <avr/iom168p.h> */
-/* #elif defined (__AVR_ATmega328P__) */
-/* #  include <avr/iom328p.h> */
+#include <avr/sfr_defs.h>
 
 #define _BV(bit) (1 << (bit))
 
@@ -22,6 +16,7 @@ extern "C" {
 
 /* , ISC10, ISC01, INT1, INT0, WGM01, WGM00, CS01, CS00, TOIE0
 , ADPS2, ADPS1, ADPS0, REFS0, ADATE, ADEN, ADSC, ADIE */
+  extern uint8_t UBRR0H, UBRR0L, UCSR0B, UCSR0A, UDR0;
   extern uint8_t TCCR0A, TCCR0B, TIMSK0;
   extern uint8_t TIMSK1, TCCR1A, TCCR1B, OCR1A, OCR1B;
   extern uint8_t TIMSK2, TCCR2A, TCCR2B, OCR2A, OCR2B;
@@ -31,15 +26,11 @@ extern "C" {
   extern uint8_t DDRB, DDRC, DDRD;
   extern uint8_t PORTB, PORTC, PORTD;
 
-/*   extern uint8_t PINB, PINC, PIND; */
+  extern uint8_t SREG; // System Register
+
 #define PINB PORTB
 #define PINC PORTC
 #define PIND PORTD
-
-/*   extern uint8_t UDR0; */
-/*   extern uint8_t PORTB0, PORTB1, PORTB2, PORTB3, PORTB4, PORTB5, PORTB6, PORTB7; */
-/*   extern uint8_t PORTC0, PORTC1, PORTC2, PORTC3, PORTC4, PORTC5, PORTC6, PORTC7; */
-/*   extern uint8_t PORTD0, PORTD1, PORTD2, PORTD3, PORTD4, PORTD5, PORTD6, PORTD7; */
 
 #ifdef __cplusplus
 }
