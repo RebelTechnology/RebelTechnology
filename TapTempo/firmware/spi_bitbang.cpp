8c1fbe84f022beb4d27a0f3899b7eec61a062632
mars <martin@rebel-it.com>
2012-11-23 10:53:42 +0000

    changed spi implementations

--- TapTempo/firmware/spi_bitbang.cpp
+++ TapTempo/firmware/spi_bitbang.cpp
@@ -1,34 +1,33 @@
 #include "spi.h"
 
-#include <wiring.h>
+#include <avr/io.h>
 
-#define CS_PIN   9       // CS slave select pin
-#define SCK_PIN  13      // SCK clock pin
-#define SDI_PIN  11      // SDI MOSI pin: master out, slave in
+#define SCK_DDR  DDRB
+#define SCK_PIN  PB5      // SCK clock pin
+#define SCK_PORT PORTB
+#define SDI_DDR  DDRB
+#define SDI_PIN  PB3      // SDI MOSI pin: master out, slave in
+#define SDI_PORT PORTB
 
-static void spi_transfer(uint8_t working) {
-  // function to actually bit shift the data byte out
-  for(int i = 1; i <= 8; i++) {
-    if(working > 127)
-      digitalWrite (SDI_PIN, HIGH); // if it is a 1 (ie. B1XXXXXXX), set the master out pin high
+static void spi_transfer(uint8_t value) {
+  for(uint8_t i=0x80; i; i>>=1){
+    if(value & i)
+      SDI_PORT |= _BV(SDI_PIN);
     else
-      digitalWrite (SDI_PIN, LOW); // if it is not 1 (ie. B0XXXXXXX), set the master out pin low
-    digitalWrite (SCK_PIN, HIGH); // set clock high, the pot IC will read the bit into its register
-    working = working << 1;
-    digitalWrite(SCK_PIN, LOW); // set clock low, the pot IC will stop reading and prepare for the next iteration (next significant bit
+      SDI_PORT &= ~_BV(SDI_PIN);
+    // clock
+    SCK_PORT |= _BV(SCK_PIN); // HIGH
+    SCK_PORT &= ~_BV(SCK_PIN); // LOW
   }
 }
 
-static void spi_out(uint8_t high, uint8_t low) {
-  digitalWrite(CS_PIN, LOW);
+void spi_out(uint8_t high, uint8_t low) {
   spi_transfer(high);
   spi_transfer(low);
-  digitalWrite(CS_PIN, HIGH);
 }
 
-static void spi_init() {
-  pinMode(CS_PIN, OUTPUT); // set CS pin to output
-  pinMode(SCK_PIN, OUTPUT); // set SCK pin to output
-  pinMode(SDI_PIN, OUTPUT); // set MOSI pin to output
-  digitalWrite(CS_PIN, HIGH); // hold slave select 1 pin high, so that chip is not selected to begin with
+void spi_init(){
+  // set pins to output
+  SCK_DDR |= _BV(SCK_PIN);
+  SDI_DDR |= _BV(SDI_PIN);
 }
--- TapTempo/firmware/spi_hw.cpp
+++ TapTempo/firmware/spi_hw.cpp
@@ -9,25 +9,20 @@
 #define SDI_PIN  PB3      // SDI MOSI pin: master out, slave in
 #define SDI_PORT PORTB
 
+void spi_init(){
+  // set pins to output
+  SCK_DDR |= _BV(SCK_PIN);
+  SDI_DDR |= _BV(SDI_PIN);
+  // Enable SPI, Master, set clock rate fck/16 
+  SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPR0);
+}
+
 static void spi_transfer(uint8_t value) {
-  for(uint8_t i=0x80; i; i>>=1){
-    if(value & i)
-      SDI_PORT |= _BV(SDI_PIN);
-    else
-      SDI_PORT &= ~_BV(SDI_PIN);
-    // clock
-    SCK_PORT |= _BV(SCK_PIN); // HIGH
-    SCK_PORT &= ~_BV(SCK_PIN); // LOW
-  }
+  SPDR = value;
+  while(!(SPSR & _BV(SPIF))); 
 }
 
 void spi_out(uint8_t high, uint8_t low) {
   spi_transfer(high);
   spi_transfer(low);
-}
-
-void spi_init(){
-  // set pins to output
-  SCK_DDR |= _BV(SCK_PIN);
-  SDI_DDR |= _BV(SDI_PIN);
 }
