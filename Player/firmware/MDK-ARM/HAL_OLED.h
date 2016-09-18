257601a99f87e311c4be4f4f0aa1e0f7e4f8e1af
collijp1 <collijp1@gmail.com>
2016-09-18 19:57:27 +0100

    Added pixel manipulation functions to OLED.c library, increased speed of spi, split write functions into 2 for Command or Data

--- Player/firmware/MDK-ARM/HAL_OLED.h
+++ Player/firmware/MDK-ARM/HAL_OLED.h
@@ -1,5 +1,6 @@
 #include "stm32f7xx_hal.h"
 
+// _____ Defines _______________________________________________________________________
 #define pRST_Set()	HAL_GPIO_WritePin(OLED_RST_GPIO_Port, OLED_RST_Pin, GPIO_PIN_SET)
 #define pDC_Set()		HAL_GPIO_WritePin(OLED_DC_GPIO_Port, 	OLED_DC_Pin, 	GPIO_PIN_SET)
 #define pCS_Set()		HAL_GPIO_WritePin(OLED_CS_GPIO_Port, 	OLED_CS_Pin, 	GPIO_PIN_SET)
@@ -11,13 +12,19 @@
 #define OLED_DAT	1
 #define OLED_CMD	0
 
-void OLED_writeData(uint16_t type, const uint8_t* data, uint16_t length);
-unsigned char OLED_readByte(uint8_t address);
+// _____ Prototypes ____________________________________________________________________
+void OLED_writeCMD(const uint8_t* data, uint16_t length);
+void OLED_writeDAT(const uint8_t* data, uint16_t length);
+uint8_t OLED_getPixel(uint8_t x, uint8_t y);
+void OLED_setPixel(uint8_t x, uint8_t y);
+void OLED_clearPixel(uint8_t x, uint8_t y);
+void OLED_togglePixel(uint8_t x, uint8_t y);
 void OLED_Refresh(void);
 void OLED_ClearScreen(void);
 void OLED_Config(void);
 void NopDelay(uint32_t nops);
 
+// _____ Variables _____________________________________________________________________
 extern unsigned char OLED_Buffer[1024];
 
 static const uint8_t OLED_initSequence[] = 
