e3ed7f23e2601e96991caca641335c200b708569
mars <martin@rebel-it.com>
2017-02-16 15:11:14 +0000

    linear envelopes functional

--- SlopeGenerator/firmware/Source/SlopeGenerator.cpp
+++ SlopeGenerator/firmware/Source/SlopeGenerator.cpp
@@ -1,9 +1,18 @@
 #include <stdlib.h>
 #include "main.h"
 #include "stm32f1xx_hal.h"
-#include "DDS.h"
 #include "gpio.h"
 #include "periph.h"
+
+#define ATTACK1  2
+#define ATTACK2  3
+#define RELEASE1 4
+#define RELEASE2 5
+#define SHAPE1   0
+#define SHAPE2   1
+
+// #define DEBUG_PINS_CLOCK
+// #define DEBUG_PINS_MAIN
 
 extern "C"{
   void setup();
@@ -21,41 +30,71 @@ enum Channel {
 };
 
 enum OperatingMode {
-  OFF, UP, DOWN
+  CYCLE_MODE, GATE_MODE, TRIGGER_MODE
 };
 
 OperatingMode getMode(Channel ch){  
   switch(ch){
   case CH1:
     if(!getPin(SW1A_GPIO_Port, SW1A_Pin))
-      return UP;
+      return CYCLE_MODE;
     else if(!getPin(SW1B_GPIO_Port, SW1B_Pin))
-      return DOWN;
+      return TRIGGER_MODE;
     else
-      return OFF;
+      return GATE_MODE;
   case CH2:
     if(!getPin(SW2A_GPIO_Port, SW2A_Pin))
-      return UP;
+      return CYCLE_MODE;
     else if(!getPin(SW2B_GPIO_Port, SW2B_Pin))
-      return DOWN;
+      return TRIGGER_MODE;
     else
-      return OFF;
+      return GATE_MODE;
   }
-  return OFF;
+  return GATE_MODE;
 }
 
-#define NOF_ADC_VALUES 12
-uint16_t adc_values[NOF_ADC_VALUES];
-uint16_t dac_values[4];
+uint16_t adc_values[6];
+uint16_t dac_values[2];
+
 uint16_t getAnalogValue(uint8_t index){
   return adc_values[index];
 }
+
 void setAnalogValue(Channel ch, uint16_t value){
   value &= 0xfff;
   if(ch == CH1)
     dac_values[0] = value;
   else if(ch == CH2)
     dac_values[1] = value;
+}
+
+// template<Channel CH>
+// void setLed(uint16_t brightness);
+
+void setLed(Channel ch, uint16_t brightness){
+  switch(ch){
+  case CH1:
+    setLed1(brightness);
+    break;
+  case CH2:
+    setLed2(brightness);
+    break;
+  }
+}
+
+// template<Channel CH>
+// bool isTriggerHigh();
+
+bool isTriggerHigh(Channel ch){
+  if(ch == CH1)
+    return !getPin(TR1_GPIO_Port, TR1_Pin);
+  return !getPin(TR2_GPIO_Port, TR2_Pin);
+}
+
+bool isTriggerLow(Channel ch){
+  if(ch == CH1)
+    return getPin(TR1_GPIO_Port, TR1_Pin);
+  return getPin(TR2_GPIO_Port, TR2_Pin);
 }
 
 /* period 3000: 1kHz toggle
@@ -79,235 +118,160 @@ void setAnalogValue(Channel ch, uint16_t value){
 if(getSysTicks() < nm ## Debounce+(ms)) return; nm ## Debounce = getSysTicks();} */
 
 
-inline bool isSlowMode(){
-  return !getPin(SW1A_GPIO_Port, SW1A_Pin);
-}
+enum Stage {
+  ATTACK_STAGE, SUSTAIN_STAGE, RELEASE_STAGE, END_STAGE
+};
 
-inline bool isFastMode(){
-  return !getPin(SW1B_GPIO_Port, SW1B_Pin);
-}
+#define MAX_LEVEL (3960<<18) // 3960 appears to be at DAC maximum excursion
+#define MIN_LEVEL 0
 
-inline bool isSineMode(){
-  return !getPin(SW2A_GPIO_Port, SW2A_Pin);
-}
-
-inline bool isTriangleMode(){
-  return !getPin(SW2B_GPIO_Port, SW2B_Pin);
-}
-
-class Synchroniser {
-private:
-  volatile uint32_t trig;
-  uint32_t period;
-  bool isHigh;  
-  OperatingMode mode;
+template<Channel CH>
+class Envelope {
 public:
-  uint16_t speed;
-  DDS dds;
-  uint32_t sine, saw;
-public:
-  Synchroniser() : trig(TRIGGER_LIMIT), period(0),
-	       isHigh(false), mode(OFF), speed(4095) {
-    dds.setPeriod(period);
-  }
-  void reset(){
-    trig = TRIGGER_LIMIT;
-    sine = saw = 0; // reset phase accumulators
-    setAnalogValue(CH1, DDS_RAMP_ZERO_VALUE);
-    setAnalogValue(CH2, DDS_SINE_ZERO_VALUE);
-    // DAC_SetDualChannelData(DAC_Align_12b_R, DDS_RAMP_ZERO_VALUE, DDS_SINE_ZERO_VALUE);
+  int32_t level;
+  OperatingMode mode;  
+  uint32_t attack;
+  uint32_t release;
+  Stage stage;
+  Envelope() : 
+    level(0), mode(GATE_MODE), attack(0), release(0), stage(END_STAGE) {}
+  void clock(){
+    switch(stage){
+    case ATTACK_STAGE:
+      level += attack;
+      if(level >= MAX_LEVEL){
+	level = MAX_LEVEL;
+	stage = SUSTAIN_STAGE;
+      }
+      break;
+    case SUSTAIN_STAGE:
+      if(mode != GATE_MODE || isTriggerLow(CH))
+	stage = RELEASE_STAGE;
+      break;
+    case RELEASE_STAGE:
+      level -= release;
+      if(level <= MIN_LEVEL){
+	level = MIN_LEVEL;
+	stage = END_STAGE;
+      }
+      break;
+    case END_STAGE:
+      if(mode == CYCLE_MODE)
+	stage = ATTACK_STAGE;
+    }
+    setAnalogValue(CH, level>>18);
+    setLed(CH, (level>>22)&0x7f);
   }
   void trigger(){
-    if(trig < TRIGGER_THRESHOLD)
-      return;
-    if(trig < TRIGGER_LIMIT){
-      period = trig;
-      dds.setPeriod(period);
-    }
-    trig = 0;
-    saw = 0; // phase reset
-  }
-  void setSpeed(int16_t s){
-    if(abs(speed-s) > 16){
-      int64_t delta = (int64_t)period*(speed-s)/1024;
-      period = max(1, period+delta);
-      dds.setPeriod(period);
-      speed = s;
-    }
-  }
-  void setMode(OperatingMode m){
-    if(m == OFF && mode != OFF)
-      reset();
-    mode = m;
-  }
-  void clock(){
-    if(trig < TRIGGER_LIMIT)
-      trig++;
-    if(mode == UP){
-      setAnalogValue(CH1, dds.getRisingRamp(saw));
-      setAnalogValue(CH2, dds.getSine(sine));
-      // DAC_SetDualChannelData(DAC_Align_12b_R, dds.getRisingRamp(saw), dds.getSine(sine));
-      saw += dds.inc();
-      sine += dds.inc();
-    }else if(mode == DOWN){
-      setAnalogValue(CH1, dds.getFallingRamp(saw));
-      setAnalogValue(CH2, dds.getTri(sine));
-      // DAC_SetDualChannelData(DAC_Align_12b_R, dds.getFallingRamp(saw), dds.getTri(sine));
-      saw += dds.inc();
-      sine += dds.inc();
-    }
+    stage = ATTACK_STAGE;
+    // level = MIN_LEVEL;
   }
 };
 
-class SlopeGenerator {
+class TapTempo {
 private:
   volatile uint32_t counter;
   uint32_t goLow;
   uint32_t goHigh;
   uint32_t trig;
   bool isHigh;  
-  bool on;
 public:
   uint16_t speed;
-  SlopeGenerator() : counter(0), goLow(TRIGGER_LIMIT>>2), goHigh(TRIGGER_LIMIT>>1), 
-	       trig(0), isHigh(false), on(false), speed(4095) {}	       
+  TapTempo() : counter(0), goLow(TRIGGER_LIMIT>>2), goHigh(TRIGGER_LIMIT>>1), 
+	       trig(0), isHigh(false) {}
   void reset(){
     counter = 0;
     setLow();
   }
-  void trigger(bool high){
+  void trigger(){
     if(trig < TAP_THRESHOLD)
       return;
     if(trig < TRIGGER_LIMIT){
-      if(high){
-	setHigh();
-	goHigh = trig;
-	goLow = trig>>1;
-	counter = 0;
-	trig = 0;
-      }else{
-	setLow();
-      }
-    }else if(high){
+      setHigh();
+      goHigh = trig;
+      goLow = trig>>1;
+      counter = 0;
+      trig = 0;
+    }else{
       trig = 0;
     }
-  }
-  void setSpeed(int16_t s){
-    if(abs(speed-s) > 16){
-      // int64_t delta = (int64_t)goLow*(speed-s)/2048;
-      // goLow = max(1, goLow+delta);
-      int64_t delta = (int64_t)goHigh*(speed-s)/2048;
-      goHigh = max(1, goHigh+delta);
-      goLow = goHigh >> 1;
-      speed = s;
-    }
-  }
-  void setStatus(bool enable){
-    if(!enable && on)
-      reset();
-    on = enable;
   }
   void clock(){
     if(trig < TRIGGER_LIMIT)
       trig++;
-    if(on){
-      if(++counter >= goHigh){
-	setHigh();
-	counter = 0;
-      }else if(counter >= goLow && isHigh){
-	setLow();
-      }
-      if(isSineMode())
-	setLed1(LED_FULL*(goHigh-counter)/(goHigh+1));
-      else
-	setLed1(LED_FULL*counter/(goHigh+1));
+    if(++counter >= goHigh){
+      setHigh();
+      counter = 0;
+    }else if(counter >= goLow && isHigh){
+      setLow();
     }
   }
   void setLow(){
     isHigh = false;
-    setPin(TR1_GPIO_Port, TR1_Pin);
-    setLed1(0);    
+    clearPin(TEMPOOUT_GPIO_Port, TEMPOOUT_Pin);
   }
   void setHigh(){
     isHigh = true;
-    clearPin(TR1_GPIO_Port, TR1_Pin);
-    setLed1(LED_FULL);
-  }
-  void toggle(){
-    if(isHigh)
-      setLow();
-    else
-      setHigh();
+    setPin(TEMPOOUT_GPIO_Port, TEMPOOUT_Pin);
   }
 };
 
-Synchroniser synchro;
-SlopeGenerator tempo;
-
-// todo: proper debouncing with systick counter
-// void buttonCallback(){
-//   tempo.trigger(isPushButtonPressed());
-// }
-
-// void triggerCallback(){
-//   if(isTriggerHigh()){
-//     synchro.trigger();
-//   }
-// }
+TapTempo tempo;
+Envelope<CH1> env1;
+Envelope<CH2> env2;
 
 extern "C"{
   void HAL_GPIO_EXTI_Callback(uint16_t pin){
     switch(pin){
     case TR1_Pin:
+      env1.trigger();
       break;
     case TR2_Pin:
+      env1.trigger();
       break;
     case TEMPOIN_Pin:
+      tempo.trigger();
       break;
     }
   }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-// void timerCallback(){
+#ifdef DEBUG_PINS_CLOCK
+  setPin(TEMPOOUT_GPIO_Port, TEMPOOUT_Pin);
+#endif
   tempo.clock();
-  synchro.clock();
-#ifdef DEBUG_PINS
-  togglePin(GPIOB, GPIO_Pin_10); // debug
+  env1.clock();
+  env2.clock();
+#ifdef DEBUG_PINS_CLOCK
+  clearPin(TEMPOOUT_GPIO_Port, TEMPOOUT_Pin);
 #endif
 }
+
+  // void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
+  // }
 
   void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *hadc){
     assert_param(0);
   }
-  // void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
-  // }
-
   void HAL_DAC_ErrorCallbackCh1(DAC_HandleTypeDef *hdac){
     assert_param(0);
   }
-
   void HAL_DAC_ErrorCallbackCh2(DAC_HandleTypeDef *hdac){
     assert_param(0);
   }
 }
 
 void updateMode(){
-  if(isSineMode()){
-    synchro.setMode(UP);
-    tempo.setStatus(true);
-  }else if(isTriangleMode()){
-    synchro.setMode(DOWN);
-    tempo.setStatus(true);
-  }else{
-    synchro.setMode(OFF);
-    tempo.setStatus(false);
-  }
+  env1.mode = getMode(CH1);
+  env2.mode = getMode(CH2);
 }
 
-void updateSpeed(){
-  int16_t p = getAnalogValue(0);
-  synchro.setSpeed(p);
-  tempo.setSpeed(p);
+#define MIN_SLOPE 19280
+void updateParameters(){
+  env1.attack = (getAnalogValue(ATTACK1)<<10L) + MIN_SLOPE;
+  env2.attack = (getAnalogValue(ATTACK2)<<10L) + MIN_SLOPE;
+  env1.release = (getAnalogValue(RELEASE1)<<10L) + MIN_SLOPE;
+  env2.release = (getAnalogValue(RELEASE2)<<10L) + MIN_SLOPE;
 }
 
 void setup(){
@@ -330,13 +294,17 @@ void setup(){
   setLed1(LED_FULL/2);
   setLed2(LED_FULL/2);
 
-  updateSpeed();
-  synchro.speed = getAnalogValue(0);
-  tempo.speed = getAnalogValue(0);
+  updateMode();
+  updateParameters();
 }
 
-
 void loop(){
+#ifdef DEBUG_PINS_MAIN
+  setPin(TEMPOOUT_GPIO_Port, TEMPOOUT_Pin);
+#endif
   updateMode();
-  updateSpeed();
+  updateParameters();
+#ifdef DEBUG_PINS_MAIN
+  clearPin(TEMPOOUT_GPIO_Port, TEMPOOUT_Pin);
+#endif
 }
